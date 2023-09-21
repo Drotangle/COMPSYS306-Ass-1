c02b880a5cc379462df2f6dbcab9ff8a406b1dc5
Jake <jbow177@aucklanduni.ac.nz>
2023-09-21 16:42:20 +1200

    minor change and added git ignore

--- COMPSYS 306 - Assignment Part 1/main.py
+++ COMPSYS 306 - Assignment Part 1/main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+# first have a look at the data
+# and put it into a dataframe
+import pandas as pd
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+autodataset = '/content/gdrive/MyDrive/CS306_2023/database/numerical/auto-mpg.csv'
+datafr = pd.read_csv(autodataset)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(datafr.shape)
+print(datafr.size)
